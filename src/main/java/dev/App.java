0153614d494f581235a4e780b63ea39163f585a0
Clement Littardi <littardi.clement@gmail.com>
2019-11-12 18:03:23 +0100

    tp03 : maven : etape 3

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,8 @@
 package dev;
 
+import java.util.ResourceBundle;
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
-}
+    	  // récupération du titre depuis le fichier de propriété.
+        String titre = ResourceBundle.getBundle("application").getString("titre"); 
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
+        
+     // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
+     }
+   }
